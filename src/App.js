b5bd55688e99813f997da415de7a2d7e455f8a06
Tanvi Pandey <tannypandey@gmail.com>
2023-08-01 12:52:37 +0100

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+          <>
+              
+                    <Navbar expand="lg" className="bg-body-tertiary">
+                              <Container>
+                                          <Navbar.Brand href="/">Heroes</Navbar.Brand>
+                                          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                          <Navbar.Collapse id="basic-navbar-nav">
+                                          <Nav className="me-auto">
+                                          <Nav.Link href="/allheroes">All Heroes</Nav.Link>
+                                          <Nav.Link href="/addhero">Add Hero</Nav.Link>
+                                          </Nav></Navbar.Collapse></Container>      </Navbar>
+                                                                                                                                    
+                                          <Routes>
+                                            <Route path="/allheroes" element={<AllHeroes/>} />  
+                                            <Route path="/addhero" element={<HeroForm/>} />
+                                            <Route path="/" element={<AllHeroes/>} />
+                                        </Routes>      </>
+            );
+                                                                                                                                                                                          
 }
 
-export default App;
+export default App
